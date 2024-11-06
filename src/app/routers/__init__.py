f9412bfea57894da750c5f789afba79726cdc1ee
Dominic Doringo <dominicdoringo619@gmail.com>
2024-11-06 00:14:37 -0800

    changed routes

--- src/app/routers/__init__.py
+++ src/app/routers/__init__.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter
 
-from app.routes import category, user
+from app.routers import category, users
 
 api_router = APIRouter()
 
-api_router.include_router(user.router, prefix="/users", tags=["Users"])
+api_router.include_router(users.router, prefix="/users", tags=["Users"])
 api_router.include_router(category.router, prefix="/categories", tags=["Categories"])
