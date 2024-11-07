5c7f34b523b6c01b7df863fd8779f07ebf8709b4
Dominic Doringo <dominicdoringo619@gmail.com>
2024-11-07 05:25:18 -0800

    Created routes for order. Order API Endpoint complete, now going to test

--- src/app/routes/__init__.py
+++ src/app/routes/__init__.py
@@ -1,11 +1,12 @@
 # routes/__init__.py
 from fastapi import APIRouter
 
-from app.routes import category, user, product, cart
+from app.routes import category, user, product, cart, order
 
 api_router = APIRouter()
 
 api_router.include_router(user.router, prefix="/users", tags=["Users"])
 api_router.include_router(product.router, prefix="/products", tags=["Products"])
 api_router.include_router(cart.router, prefix="/cart", tags=["Cart"])
+api_router.include_router(order.router, prefix="/orders", tags=["Orders"])
 api_router.include_router(category.router, prefix="/categories", tags=["Categories"])
