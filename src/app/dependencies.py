40c8d7c514a8c3b45bad3ddbff4393adcc7ec411
Dominic Doringo <dominicdoringo619@gmail.com>
2024-11-08 00:53:27 -0800

    reverted back that change

--- src/app/dependencies.py
+++ src/app/dependencies.py
@@ -1,11 +1,7 @@
-# src/app/dependencies.py
-
 from app.core.database import SessionLocal
-from sqlalchemy.orm import Session
-from fastapi import Depends
 
 
-def get_db() -> Session:
+def get_db():
     db = SessionLocal()
     try:
         yield db
