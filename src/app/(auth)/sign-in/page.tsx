627f76a76ca26eade20c6bbc307265ddc22310f0
Ugur Emre Dogan <ue.dogan@gmail.com>
2024-11-25 20:34:26 -0800

    Added sign in page.

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import { LoginForm } from '@/components/LoginForm';
+
+function SignInPage() {
+	return (
+		<div className="flex h-screen w-full items-center justify-center px-4">
+			<LoginForm />
+		</div>
+	);
+}
+export default SignInPage;
