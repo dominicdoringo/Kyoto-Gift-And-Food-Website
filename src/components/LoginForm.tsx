251b73c7ea726210fd90893317707587eb9c55e3
James Burton <themeava@outlook.com>
2024-11-28 12:32:07 -0800

    Fixed the min length for Password check to the desired minimum requirement

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -25,7 +25,7 @@ const loginSchema = z.object({
 		.min(3, { message: 'Username must be at least 3 characters long' }),
 	password: z
 		.string()
-		.min(3, { message: 'Password must be at least 8 characters long' }),
+		.min(8, { message: 'Password must be at least 8 characters long' }),
 });
 
 type LoginFormValues = z.infer<typeof loginSchema>;
